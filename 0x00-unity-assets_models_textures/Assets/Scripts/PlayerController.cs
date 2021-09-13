57b26920d74996caf51cf8fe8bcfd80032f5eb0e
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-09-13 13:16:04 -0500

    PlayerUpdates

---
+++ 0x00-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+    public class PlayerController : MonoBehaviour
+    {
+		// Controls of player movement
+        public GameObject Player;
+		private Rigidbody rb;
+		public float speed = 10.0F;
+		public float JumpSpeed = 10.0F;
+		public float Gravity = -10.0F;
+		public float JumpHeight = 5.0F;
+
+		// Player control
+		public bool isGround;
+		private Vector3 control;
+
+		// To be called at first frame update
+		void Start()
+		{
+			rb = GetComponent<Rigidbody>();
+			isGround = true;
+		}
+
+		// controls Movement of player
+		void FixedUpdate()
+		{
+			if (isGround == true)
+			{	
+				rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Jump") * JumpHeight, Input.GetAxis("Vertical") * speed);
+			}
+		}
+
+
+
+
+
+
+
+
+    }
