d3297744b4e08e021f5980c7aae359cc0c3cdff2
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-09-14 12:37:37 -0500

    Camera

---
+++ 0x00-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+		public float speedX = 2.0F;
+		public float speedY = 2.0F;
+		private float x = 0.0F;
+		private float y = 0.0F;
+
+        void Start()
+        {
+			
+        }
+
+		void Update()
+		{
+			x += speedX * Input.GetAxis("Mouse X");
+			y -= speedY * Input.GetAxis("Mouse Y");
+
+			transform.eulerAngles = new  Vector3(y, x, 0.0F);
+		}
+}
