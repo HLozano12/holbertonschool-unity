dc4a433019684e12fe1791b5bd21fa3c12870857
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-09-15 23:39:03 -0500

    TImer

---
+++ 0x00-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    
+    public Text TimerText;
+    public float currentTime;
+    public bool StopWatch = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimerText.text = "0:00.00";
+        currentTime = 0;
+    }
+
+    void Update()
+    {
+        if (StopWatch == true)
+        {
+            currentTime += Time.deltaTime;
+        }
+
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+
+        TimerText.text = time.ToString(@"m\:ss\.ff");
+    }
+
+    public void StartTime()
+    {
+        StopWatch = true;
+    }
+
+    public void StopTime()
+    {
+        StopWatch = false;
+    }
+
+
+}
