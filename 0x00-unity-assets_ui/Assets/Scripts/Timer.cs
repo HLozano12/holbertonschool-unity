bd718ce3120fbd31d8085b574b487efc84b8beee
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-10-03 16:34:15 -0500

    Timer Add

--- 0x00-unity-assets_ui/Assets/Scripts/Timer.cs
+++ 0x00-unity-assets_ui/Assets/Scripts/Timer.cs
@@ -1,31 +1,45 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Timer : MonoBehaviour
 {
-    
-    public Text TimerText;
-    public float currentTime;
+    ///<summary>Updates timer after player starts to move</summary>
 
+    public Text TimerText;
+    private float secondsCount = 00.00F;
+    private int minutesCount = 0;
+    public bool isOnPause = false;
+    public Text WinnerText;
 
     // Start is called before the first frame update
     void Start()
     {
-        TimerText.text = "0:00.00";
-        currentTime = 0;
+        
     }
 
-    void Update()
+    // Update is called once per frame
+    void FixedUpdate()
     {
-        currentTime += Time.deltaTime;
-     
-        TimeSpan time = TimeSpan.FromSeconds(currentTime);
-
-        TimerText.text = time.ToString(@"m\:ss\.ff");
+        UpdateTimerUI();
     }
 
-
+    public void UpdateTimerUI()
+    {
+        secondsCount += Time.deltaTime;
+        TimerText.text = minutesCount + ":" + secondsCount.ToString("00.00");
+        if (secondsCount >= 60)
+        {
+            minutesCount++;
+            secondsCount = 0;
+        }
+    }
+    public void Win()
+    {
+        isOnPause = true;
+        WinnerText.text = TimerText.text;
+        TimerText.text = "";
+    }
 }
