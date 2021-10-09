fe7cd70a053dc4dad1a5dd0a6bff36119aedcf5e
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-10-09 14:47:03 -0500

    Final Push

--- 0x00-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
+++ 0x00-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class OptionsMenu : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
     public void Back()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-    }
-    public void Apply()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(PlayerPrefs.GetInt("Back"));
     }
 }
